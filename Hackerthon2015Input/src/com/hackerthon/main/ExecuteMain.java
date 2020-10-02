6e5e9c51cc18d102e19e1ba247b178eea6794319
RahulFernando <rahulrulz680@gmail.com>
2020-10-02 14:46:09 +0530

    logger

--- Hackerthon2015Input/src/com/hackerthon/main/ExecuteMain.java
+++ Hackerthon2015Input/src/com/hackerthon/main/ExecuteMain.java
@@ -1,5 +1,9 @@
 package com.hackerthon.main;
 
+import java.util.concurrent.AbstractExecutorService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.hackerthon.common.UtilTransform;
 import com.hackerthon.service.EmpService;
 
@@ -9,6 +13,8 @@ public class ExecuteMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		Logger log = Logger.getLogger(AbstractExecutorService.class.getName());
 
 		EmpService employeeService = EmpService.getInstance();
 		try {
@@ -20,6 +26,7 @@ public class ExecuteMain {
 //			employeeService.EMPLOYEEDELETE("EMP10001");
 			employeeService.employeeDisplay();
 		} catch (Exception e) {
+			log.log(Level.SEVERE, e.getMessage());
 		}
 
 	}
