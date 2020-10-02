5e089c9d31f0fd118c2d826cea46dc5c79ca08ff
plhewa <plhewa2@gmail.com>
2020-10-02 05:51:34 +0530

    adding comments to the GlobalConstant.java

--- Hackerthon2015Input/src/com/hackerthon/config/GlobalConstant.java
+++ Hackerthon2015Input/src/com/hackerthon/config/GlobalConstant.java
@@ -1,32 +1,56 @@
 package com.hackerthon.config;
 
 public class GlobalConstant {
+	//constant to read "XpathEmployeeIDKey" 
 	public static final String X_PATH_EMPLOYEE_ID_KEY = "XpathEmployeeIDKey";
+	//constant to read "XpathEmployeeNameKey" 
 	public static final String X_PATH_EMPLOYEE_NAME_KEY = "XpathEmployeeNameKey";
+	//constant to read "XpathEmployeeAddressKey" 
 	public static final String X_PATH_EMPLOYEE_ADDRESS_KEY = "XpathEmployeeAddressKey";
+	//constant to read "XpathFacultyNameKey" 
 	public static final String X_PATH_FACULTY_NAME_KEY = "XpathFacultyNameKey";
+	//constant to read "XpathDepartmentKey"
 	public static final String X_PATH_DEPARTMENT_KEY = "XpathDepartmentKey";
+	//constant to read XpathDesignationKey
 	public static final String X_PATH_DESIGNATION_KEY ="XpathDesignationKey";
 	
+	//constant to read q1 
 	public static final String Q1 ="q1";
+	//constant to read q2
 	public static final String Q2 ="q2";
+	//constant to read q3
 	public static final String Q3 ="q3";
+	//constant to read q4
 	public static final String Q4 ="q4";
+	//constant to read q5
 	public static final String Q5 ="q5";
+	//constant to read q6
 	public static final String Q6 ="q6";
 	
+	//constant to read 1
 	public static final int ONE = 1;
+	//constant to read 2
 	public static final int TWO = 2;
+	//constant to read 3
 	public static final int THREE = 3;
+	//constant to read 4
 	public static final int FOUR = 4;
+	//constant to read 5
 	public static final int FIVE = 5;
+	//constant to read 6
 	public static final int SIX = 6;
 	
+	//constant to read Employee Id field
 	public static final String EMPID = "Employee ID";
+	//constant to read Full Name field
 	public static final String FULL_NAME = "FullName";
+	//constant to read Address field
 	public static final String ADDRESS = "Address";
+	//constant to read Faculty Name field
 	public static final String FACULTY_NAME = "Faculty Name";
+	//constant to read Department field
 	public static final String DEPARTMENT = "Department";
+	//constant to read Designation
 	public static final String DESIGNATION = "Designation";
 	
 	//constant to read property file for url
@@ -36,9 +60,14 @@ public class GlobalConstant {
 	//constant to read property file for password
 	public static final String PASSWORD = "password";
 	
+	//constant to read config.properties file path
 	public static final String CONFIG_PROP_PATH = "../config/config.properties";
+	//constant to read EmployeeQuery.xml file path
 	public static final String CONFIG_EMP_QUERY_XML = "src/com/hackerthon/config/EmployeeQuery.xml";
+	//constant to read EmployeeRequest.xml file path
 	public static final String CONFIG_EMP_REQ_XML = "src/com/hackerthon/config/EmployeeRequest.xml";
+	//constant to read Employee-modified.xsl file path
 	public static final String CONFIG_EMP_MODIFIED_XSL = "src/com/hackerthon/config/Employee-modified.xsl";
+	//constant to read EmployeeResponse.xml file path
 	public static final String CONFIG_EMP_RESPONSE_XML = "src/com/hackerthon/config/EmployeeResponse.xml";
 }
